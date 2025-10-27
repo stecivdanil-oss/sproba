7442101eca7c5b1b41eb0545c8c93661e3e0eed7
stecivdanil-oss <stecivdanil@gmail.com>
2025-10-27 20:46:25 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+import Heder from './components/Heder.vue';
+
+
+let info = 'mycompany'
+let clicks = ref(0)
+let name = ref('')
+let check = ref(true)
+
+</script>
+
+<template>
+<Heder :name = "info" :count = "clicks" />
+
+<h1 :class="check?'h1':''">hello {{ name }}</h1>
+<button @click="clicks +=1" >aaa</button>
+<button @click = " check = !check">change color</button>
+<div v-show = "check">
+  <img src="./assets/vue.svg" alt="">
+</div>
+<input type="text" v-model="name">
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 40px;
+  font-family: sans-serif;
+}
+.h1{
+  background-color: orange;
+}
+</style>
---
+++ src/components/Heder.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+    name: String,    
+    count: Number
+})
+</script>
+
+<template>
+<header>
+    <div class="conteiner">
+        <div class="menu">
+            <span>{{name}}</span>
+            <nav>
+                <a href="">про нас</a>
+                <a href="">товар</a>
+                <a href="">відгуки {{ count }}</a>
+            </nav>
+        </div>
+    </div>
+    
+</header>
+</template>
+
+<style scoped>
+header{
+    background-color: blueviolet;
+
+}
+.conteiner{
+    max-width: 900px;
+    margin: 0 auto;
+}
+.menu{
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+}
+</style>
