20bdc5ecc1178ba018140232610db4046f84091d
stecivdanil-oss <stecivdanil@gmail.com>
2025-10-31 20:51:45 +0200

    a
    aa

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,13 @@ let clicks = ref(0)
 let name = ref('')
 let check = ref(true)
 
+import { products } from './Data';
+import Card from './components/Card.vue';
+let data =products
+
+
+
+
 </script>
 
 <template>
@@ -22,6 +29,9 @@ let check = ref(true)
 </div>
 <input type="text" v-model="name">
  <ItemCard/>
+ <div class="conteiner">
+  <Card v-for="el in data" :item =  "el" />
+ </div>
 </template>
 
 <style scoped>
@@ -32,5 +42,14 @@ let check = ref(true)
 }
 .h1{
   background-color: orange;
+}
+.conteiner{
+    max-width: 900px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    flex-wrap: wrap;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+   item:Object
+
+})
+</script>
+
+<template>
+<div class="card">
+<img :src="item.image " alt="">
+<h3>name:{{ item.name }}</h3>
+<p>price:{{ item.name }}</p>
+</div>
+</template>
+
+<style scoped>
+.card{
+    width: calc(890px / 3);
+    height: 330px;
+    position: relative;
+}
+img{
+    width: 100%;
+    height: 210px;
+
+}
+h3,p{
+    text-align: center;
+}
+</style>
